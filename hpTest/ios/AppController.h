7ffdea4f98af84b807dbe0fe107ed1c7fd9e2a36
beyonehu <beyonehu@beyonehutekiMac-mini.local>
2014-01-12 01:59:19 +0800

    Initial Commit

---
+++ hpTest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  hpTestAppController.h
+//  hpTest
+//
+//  Created by beyonehu on 14-1-12.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ hpTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  hpTestAppController.h
+//  hpTest
+//
+//  Created by beyonehu on 14-1-12.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
